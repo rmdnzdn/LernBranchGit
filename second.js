19973172a54935fb36da567f122c016cc0cc17af
Muhammad Ramdan(Zidun) <ramdanzidun@gmail.com>
2019-12-04 07:16:33 +0700

    commit pertama

---
+++ second.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class second extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> second </Text>
+      </View>
+    );
+  }
+}
+
+export default second;
