8e207c8482b5b09e62ad556280d70efbd85588d3
Muhammad Ramdan(Zidun) <ramdanzidun@gmail.com>
2019-12-04 07:59:33 +0700

    add cabang screen.js

---
+++ cabang.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class cabang  extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> cabang  </Text>
+      </View>
+    );
+  }
+}
+
+export default cabang ;
