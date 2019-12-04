06dd83ff4d569fade3d42bb7d17aa76208971a2f
Muhammad Ramdan(Zidun) <ramdanzidun@gmail.com>
2019-12-04 08:07:55 +0700

    add devvelopment file

---
+++ development.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class development extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> development </Text>
+      </View>
+    );
+  }
+}
+
+export default development;
