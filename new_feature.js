07a2b829ffe4dbe0456de0779c356d06ccb6c41c
Muhammad Ramdan(Zidun) <ramdanzidun@gmail.com>
2019-12-05 20:40:59 +0700

    Adding New Feature

---
+++ new_feature.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class new_feature extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> new_feature </Text>
+      </View>
+    );
+  }
+}
+
+export default new_feature;
