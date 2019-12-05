a1a79e057a4c357b13cbfc85200c4a106a037e30
Muhammad Ramdan(Zidun) <ramdanzidun@gmail.com>
2019-12-05 21:14:36 +0700

    adding feature_branch_name

---
+++ feature_branch_name.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class feature_branch_name extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> feature_branch_name </Text>
+      </View>
+    );
+  }
+}
+
+export default feature_branch_name;
